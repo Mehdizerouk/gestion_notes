b12afc9de08892d4e7703daf79cbd1a711b853d0
mehdiz <mehdiz@DESKTOP-I2TQVV9>
2022-04-15 15:42:30 +0000

    Methode ADD class ListEtudiants

--- gestion_notes/src/gestion_notes/ListEtudiants.java
+++ gestion_notes/src/gestion_notes/ListEtudiants.java
@@ -1,7 +1,17 @@
 package gestion_notes;
 import java.util.ArrayList;
 
+
 public class ListEtudiants extends ArrayList<Etudiant> {
+	@Override
+	public boolean add(Etudiant e) {
+		if(this.contains(e)) {
+			int index = indexOf(e);
+			set(index,e);
+			return true;
+		}
+			return super.add(e);
+	}
 	
 
 }
