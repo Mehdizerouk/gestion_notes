2c299e623cc7d3b5feffe3c4de2712df69a28195
mehdiz <mehdiz@DESKTOP-I2TQVV9>
2022-04-15 15:29:09 +0000

    Methode CompareTo

--- gestion_notes/src/gestion_notes/Etudiant.java
+++ gestion_notes/src/gestion_notes/Etudiant.java
@@ -1,13 +1,13 @@
 package gestion_notes;
 
-import aiac.gi18.java.complexe.Complexe;
+import gestion_notes.Etudiant;
 
-public class Etudiant {
+public class Etudiant implements Comparable<Etudiant>{
 	private int id;
 	private String nom;
 	private double note;
 	
-	public Etudiant(int id,String nom,double note) {
+	public Etudiant(int id,String nom) {
 		this.id=id;
 		this.nom=nom;
 		this.note=10;
@@ -26,6 +26,14 @@ public class Etudiant {
 			return false;
 		}
 	return	((Etudiant)obj).id==this.id; 
+	}
+	
+	@Override
+	public int compareTo(Etudiant e) {
+		if(this.note==e.note) return 0;
+		if(this.note>e.note) return 1;
+		return -1;
+		
 	}
 	
 	
